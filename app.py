9897adc1e2d5fa4bc71cec208b85458cd6a7edb6
beli10k <isabeleferreira@colegiopaulodetarso.com.br>
2026-08-04 11:14:11 +0000

    hyiiu

--- app.py
+++ app.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/boletim")
 def boletim():
-    return render_template('boletim.html')
+    return render_template("boletim.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
